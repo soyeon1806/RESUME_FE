465289a7ca0d412aa9975d6bf91b306f982cc68d
soyeon <ksy1118s@naver.com>
2025-06-07 17:59:55 +0900

    📝 chore: main.tsx 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+
+import '@/styles/index.css';
 import App from './App.tsx';
 
+// HTML의 <div id="root"> 요소를 가져와서 그 안에 React 앱을 랜더링함
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* App 컴포넌트를 React 방식으로 랜더링 */}
     <App />
   </StrictMode>,
 );
