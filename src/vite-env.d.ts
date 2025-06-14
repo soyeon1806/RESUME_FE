51191caa8bdbd01d52f9ebec9057906e322e06b5
soyeon <102381857+soyeon1806@users.noreply.github.com>
2025-06-14 19:58:56 +0900

    📝 chore: declarations.d.ts로 타입 정의 파일 통합 (#11)

--- src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-// TypeScript가 .png 파일을 import 할 수 있도록 설정
-declare module '*.png' {
-  // png 파일을 import 하면 문자열(src 경로)로 반환된다고 명시
-  const src: string;
-
-  // 해당 경로 문자열을 기본값으로 export
-  export default src;
-}
